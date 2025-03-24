442d8ea75509be03b99cfb43c1e48fa4bb8f58b6
Ruslan Opihaylenko <rysya08shkolnik@gmail.com>
2025-03-24 18:50:11 +0200

    first commit

---
+++ C# 18.03 HW/Product.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__18._03_HW
+{
+    internal class Product
+    {
+        public Guid Id { get; init; } = Guid.NewGuid();
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Category { get; set; }
+        public List<Product> Products { get; set; }
+        public Product(Guid id, string name, double price, string category)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Category = category;
+        }
+        public void Show()
+        {
+            for (int i = 0; i < Products.Count; i++)
+            {
+                Console.WriteLine(Products[i]);
+            }
+        }
+        public void SearchName(string name)
+        {
+            for (int i = 0; i < Products.Count; i++)
+            {
+                if (name == Products[i].Name)
+                {
+                    Console.WriteLine(Products[i]);
+                }
+                else
+                {
+                    Console.WriteLine("Error");
+                }
+            }
+        }
+        public void SearchCategory(string category)
+        {
+            for (int i = 0; i < Products.Count; i++)
+            {
+                if (category == Products[i].Category)
+                {
+                    Console.WriteLine(Products[i]);
+                }
+                else
+                {
+                    Console.WriteLine("Error");
+                }
+            }
+        }
+        public void FindTheMostExpensive()
+        {
+            double max = 0;
+            for (int i = 0; i < Products.Count; i++) 
+            {
+                if (max < Products[i].Price)
+                {
+                    max = Products[i].Price;
+                }
+            }
+            Console.WriteLine(max);
+        }
+        public void Delete(Guid id)
+        {
+            for (int i = 0; i < Products.Count; i++)
+            {
+                if (id != Products[i].Id)
+                {
+                    Console.WriteLine(Products[i]);
+                }
+            }
+        }
+    }
+}
---
+++ C# 18.03 HW/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace C__18._03_HW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Guid id0 = Guid.NewGuid();
+            string name0 = "Fox";
+            string category0 = "Fox";
+            Guid id = Guid.NewGuid();
+            string name = "Fox";
+            double price = 31.23;
+            string category = "Machine";
+            Product p = new Product(id, name, price, category);
+            Guid id2 = Guid.NewGuid();
+            string name2 = "Tec";
+            double price2 = 41.25;
+            string category2 = "Cook machine";
+            Product p2 = new Product(id2, name2, price2, category2);
+            List<Product> products = new List<Product>();
+            products.Add(p);
+            products.Add(p2);
+            p.Show();
+            p.SearchName(name0);
+            p.SearchCategory(category0);
+            p.FindTheMostExpensive();
+            p.Delete(id0);
+            p2.Show();
+            p2.SearchName(name0);
+            p2.SearchCategory(category0);
+            p2.FindTheMostExpensive();
+            p2.Delete(id0);
+        }
+    }
+}
